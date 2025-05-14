f9686074cf1362efba093244cd5a028c354d3f0b
nandini <nandani@contaque.com>
2025-05-14 12:59:22 +0530

    Add these changes
    Signed-off-by: nandini <nandani@contaque.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'reusecomponent27';
+  users=['Anil','Sam','Peter','Ram','John']
 }
